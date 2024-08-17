025e458e35d9a1a2eb3a375241b47b54589a2c7a
username <usermail>
2024-08-17 17:30:09 +0530

    Done Navbar

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,6 @@
 const hamburger =document.querySelector(".hamburger");
 const navMenu =document.querySelector(".nav_menu_list");
 
-
 hamburger.addEventListener("click",() =>{
     hamburger.classList.toggle("active");
     navMenu.classList.toggle("active");
